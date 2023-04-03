1edd1655740e3c27e515563713e85a6fac8e58f6
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2023-04-03 15:21:27 -0300

    new component task-list

---
+++ JavaScript/frameworks/Angular/Tarefas/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent,
+    ShoppingListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
