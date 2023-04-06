6ef21b1996485b0fc2c32083783a8b4b54ce95fb
Wellington Macedo da Silva <wellington.macedo89@outlook.com>
2023-04-06 15:15:22 -0300

    starting angular-escola project

--- JavaScript/frameworks/Angular/angular-escola/src/app/app.component.spec.ts
+++ JavaScript/frameworks/Angular/angular-escola/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tarefas'`, () => {
+  it(`should have as title 'angular-escola'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Tarefas');
+    expect(app.title).toEqual('angular-escola');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Tarefas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-escola app is running!');
   });
 });
--- JavaScript/frameworks/Angular/angular-escola/src/app/app.module.ts
+++ JavaScript/frameworks/Angular/angular-escola/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TaskListComponent,
-    ShoppingListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
