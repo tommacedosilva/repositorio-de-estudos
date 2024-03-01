9531397735bb9ddb2ef9cb8912400f952da11e91
tommacedosilva <contatotommacedosilva@gmail.com>
2024-03-01 17:19:17 -0300

    create functions in JS

--- HTML-CSS/imersao-Alura/script.js
+++ HTML-CSS/imersao-Alura/script.js
@@ -6,7 +6,20 @@ function requestAPI(searchTerm) {
     const url = `http:localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResult(result))
+        .then((result) => displayResults(result))
+}
+
+function displayResults() {
+    resultPlaylist.classList.add('hidden')
+    const artistName = document.getElementById('artist-name')
+    const artistImage = document.getElementById('artist-img')
+
+    result.forEach(element => {
+        artistName.innerText = element.name
+        artistImage.src = element.urlImg
+    })
+
+    resultArtist.classList.remove('hidden')
 }
 
 document.addEventListener('input', () => {
