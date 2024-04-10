6c7c68c710bbefe5d9c2b76d66e2d38ca7ef9d77
tommacedosilva <contatotommacedosilva@gmail.com>
2024-04-10 17:05:17 -0300

    paused to challenges

---
+++ JavaScript/vanilla/JStoReact/logicaProg/desafios/exercicios.js
@@ -0,0 +1,22 @@
+/* 
+
+Você foi contratado(a) para desenvolver uma calculadora de média e verificar a situação de aprovação de um aluno com base em suas quatro notas.
+A média para aprovação é 5. Sua tarefa é implementar duas funções em JS
+
+- calcularMedia(nota1, nota2, nota3, nota4)=> Esta função recebe as quatro notas do aluno como parâmetros e retorna a média calculada com base nessas notas.
+
+- verificarAprovacao(media) => Esta função recebe a média do aluno como parâmetro e retorna "Aprovado" se a média for maior ou igual a 5, caso contrário, retorna "Reprovado".
+
+*/
+
+function verificarAprovacao(media) {
+    return media >= 5 ? "Aprovado" : "Reprovado"
+}
+
+function calcularMedia(nota1, nota2, nota3, nota4) {
+    let media = (nota1, nota2, nota3, nota4) / 4
+    return media 
+}
+
+
+
