d8f57672913a489f886491eb124b233e7bc432ee
tommacedosilva <contatotommacedosilva@gmail.com>
2024-05-29 10:54:10 -0300

    list of users showing name and mail

---
+++ JavaScript/frameworks/React/frontendcrud/src/components/UserList.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
+const UserList = () => {
+
+    const [users, setUsers] = useState([])
+
+    const fetchUsers = async () => {
+        try {
+            const response = await api.get('/users')
+            setUsers(response.data)
+            
+
+        } catch (error) {
+            console.error('Erro ao obter usuários:', error)
+
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    return (
+        <>
+            <h1>Lista de Usuários</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name} - {user.email}</li>
+                ))}
+
+            </ul>
+        </>
+    )
+}
+
+export default UserList
