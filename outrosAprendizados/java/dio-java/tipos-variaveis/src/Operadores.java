cbc08d0b5ef09c7e40a2b03b3d1805b8a63048bd
tommacedosilva <contatotommacedosilva@gmail.com>
2024-06-17 17:19:44 -0300

    about operators

---
+++ outrosAprendizados/java/dio-java/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //int numero = 5;
+        //System.out.println(- numero);
+        //System.out.println(numero);
+
+        //int numero = 5;
+        //System.out.println( ++ numero);
+       //System.out.println(numero);
+
+       //int a, b;
+       //a = 5;
+       //b = 6;
+       //String resultado = a==b ? "verdadeiro" : "falso";
+       //System.out.println(resultado);
+    }
+}
