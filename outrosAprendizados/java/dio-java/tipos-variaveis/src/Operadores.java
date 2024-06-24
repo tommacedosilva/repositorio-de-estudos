d9dc4e59f8c558d53e881ebbfc910de04d204798
tommacedosilva <contatotommacedosilva@gmail.com>
2024-06-24 15:03:18 -0300

    end lesson about operators

--- outrosAprendizados/java/dio-java/tipos-variaveis/src/Operadores.java
+++ outrosAprendizados/java/dio-java/tipos-variaveis/src/Operadores.java
@@ -13,5 +13,30 @@ public class Operadores {
        //b = 6;
        //String resultado = a==b ? "verdadeiro" : "falso";
        //System.out.println(resultado);
+
+       /* 
+       String nomeUm = "Tom";
+       String nomeDois = "Tom";
+
+       System.out.println(nomeUm == nomeDois);
+
+       int numero1 = 1;
+       int numero2 = 2;
+
+       boolean simNao = numero1 == numero2;
+       System.out.println("número Um é igual ao número Dois? " + simNao);
+
+       simNao = numero1 != numero2;
+       
+       System.out.println("número Um é diferente ao número Dois? " + simNao);
+       
+       boolean condicao1 = true;
+       boolean condicao2 = false;
+ 
+       if(condicao1 && condicao2) {
+        System.out.println("as duas condições são verdadeiras");
+       }
+        */
+
     }
 }
