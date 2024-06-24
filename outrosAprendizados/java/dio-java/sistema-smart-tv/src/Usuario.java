fd6e5a664c7dfe2bc68a2173e775a608f683de63
tommacedosilva <contatotommacedosilva@gmail.com>
2024-06-24 15:58:53 -0300

    project for method example

---
+++ outrosAprendizados/java/dio-java/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,21 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("TV Ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+        
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+    }
+}
