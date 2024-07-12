28e9d0b07fa477978d82aeb0d0a4283ee619aa17
tommacedosilva <contatotommacedosilva@gmail.com>
2024-07-12 14:08:50 -0300

    learning about FOR

---
+++ outrosAprendizados/java/dio-java/estrutura-repeticao/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no índice " + i + " é " + alunos[i]);
+        }
+    }
+}
