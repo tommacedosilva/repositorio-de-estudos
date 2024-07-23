c079a186dec7ba5b71378b62d661158b57e6c674
tommacedosilva <contatotommacedosilva@gmail.com>
2024-07-23 17:01:58 -0300

    getters and setters

---
+++ outrosAprendizados/java/dio-java/exemplos-pacotes/src/escola/Aluno.java
@@ -0,0 +1,31 @@
+package escola;
+
+public class Aluno {
+    private String nome;
+    private int idade;
+
+    private String sexo;
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+}
