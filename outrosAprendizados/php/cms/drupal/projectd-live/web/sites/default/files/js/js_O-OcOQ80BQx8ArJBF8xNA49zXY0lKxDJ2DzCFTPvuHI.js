47de7ec33f6667b9b6b2132027ccd9c01f8c0854
tommacedosilva <contatotommacedosilva@gmail.com>
2025-03-15 23:18:12 -0300

    created fields and labels

---
+++ outrosAprendizados/php/cms/drupal/projectd-live/web/sites/default/files/js/js_O-OcOQ80BQx8ArJBF8xNA49zXY0lKxDJ2DzCFTPvuHI.js
@@ -0,0 +1,2 @@
+/* @license GPL-2.0-or-later https://www.drupal.org/licensing/faq */
+Drupal.AjaxCommands.prototype.tabledragChanged=function(ajax,response,status){if(status!=='success')return;const tableDrag=Drupal.tableDrag[response.tabledrag_instance];const rowObject=new tableDrag.row(document.getElementById(response.id),'',tableDrag.indentEnabled,tableDrag.maxDepth,true);rowObject.markChanged();rowObject.addChangedWarning();};;
