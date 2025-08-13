347c45d73a372db780077f1e81a00124743f947c
tommacedosilva <contatotommacedosilva@gmail.com>
2025-08-13 18:38:38 -0300

    installed next-app

---
+++ JavaScript/frameworks/next/next-danvitoriano/tech40mais/src/app/conf/page.tsx
@@ -0,0 +1,7 @@
+export default function Conf() {
+  return (
+    <main>
+      <h1>Conf</h1>
+    </main>
+  );
+}
---
+++ JavaScript/frameworks/next/next-danvitoriano/tech40mais/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Tech40+</h1>
+    </main>
+  );
+}
